9e437152f3e309fe25fee4d50b49fbf5ce765e4c
Simon Taddiken <simon.taddiken@gmail.com>
2019-10-20 13:23:26 +0200

    feat(ui): Use tag input control
    Closes #19

--- tally-frontend-server/src/main/js/app.js
+++ tally-frontend-server/src/main/js/app.js
@@ -1,5 +1,7 @@
 import { DateLabel } from './date-label';
 import { InputNow } from './input-now';
+import bulmaTagsinput from '../../../node_modules/bulma-tagsinput/src/js/index.js';
 
 customElements.define("date-label", DateLabel, { extends: 'time' });
 customElements.define("input-now", InputNow, { extends: 'input' });
+bulmaTagsinput.attach();
