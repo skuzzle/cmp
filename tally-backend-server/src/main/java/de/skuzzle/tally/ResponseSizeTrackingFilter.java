c9a0ca3293beb29d0cb36111fe7db218b0b9b368
Simon Taddiken <simon.taddiken@gmail.com>
2019-10-21 17:09:15 +0200

    feat: measure response sizes
    #22

---
+++ tally-backend-server/src/main/java/de/skuzzle/tally/ResponseSizeTrackingFilter.java
@@ -0,0 +1,41 @@
+package de.skuzzle.tally;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.connector.ResponseFacade;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Tag;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class ResponseSizeTrackingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            if (response instanceof ResponseFacade) {
+                final ResponseFacade rspo = (ResponseFacade) response;
+                final long contentWritten = rspo.getContentWritten();
+                Metrics.gauge("http_server_response_bytes",
+                        List.of(Tag.of("uri", request.getRequestURI())),
+                        contentWritten);
+            }
+        }
+    }
+
+}
