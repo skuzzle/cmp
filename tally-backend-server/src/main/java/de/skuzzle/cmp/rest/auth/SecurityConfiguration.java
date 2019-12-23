f45a576004f6c6b052e4a0f11a3b95bdd43b0119
Simon Taddiken <simon.taddiken@gmail.com>
2019-12-23 18:07:43 +0100

    feat: loosen up cors config

--- tally-backend-server/src/main/java/de/skuzzle/cmp/rest/auth/SecurityConfiguration.java
+++ tally-backend-server/src/main/java/de/skuzzle/cmp/rest/auth/SecurityConfiguration.java
@@ -1,8 +1,12 @@
 package de.skuzzle.cmp.rest.auth;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -12,10 +16,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .anyRequest().permitAll().and()
-                .cors().disable()
                 .csrf().disable()
                 .oauth2ResourceServer().jwt().jwkSetUri("https://www.googleapis.com/oauth2/v3/certs").and()
                 .and()
                 .anonymous();
+    }
+
+    @Bean
+    public WebMvcConfigurer cors() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowCredentials(true)
+                        .allowedOrigins(CorsConfiguration.ALL)
+                        .allowedMethods(CorsConfiguration.ALL)
+                        .allowedHeaders(CorsConfiguration.ALL);
+            }
+        };
     }
 }
