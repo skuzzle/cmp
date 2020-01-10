474f74eeaa13b9fc9c856b7ea80f2d91f06c1566
Simon Taddiken <simon.taddiken@gmail.com>
2020-01-10 10:27:24 +0100

    feat: add simple js confirmation dialogs

--- tally-frontend-server/src/main/js/components.js
+++ tally-frontend-server/src/main/js/components.js
@@ -4,3 +4,11 @@ import { InputNow } from './components/input-now';
 
 import bulmaTagsinput from '../../../node_modules/bulma-tagsinput/src/js/index.js';
 bulmaTagsinput.attach();
+
+const elems = document.getElementsByClassName('confirmable');
+const confirmIt = function (e) {
+    if (!confirm('Are you sure? This action can not be undone')) e.preventDefault();
+};
+for (let i = 0; i < elems.length; i++) {
+    elems[i].addEventListener('click', confirmIt, false);
+}
