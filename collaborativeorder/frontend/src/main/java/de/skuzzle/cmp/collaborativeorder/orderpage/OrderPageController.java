cc85120dc80171312e2bb1a7079820a8ee92f996
Simon Taddiken <simon.taddiken@gmail.com>
2020-01-20 19:57:54 +0100

    feat(ui): template for oder page

--- collaborativeorder/frontend/src/main/java/de/skuzzle/cmp/collaborativeorder/orderpage/OrderPageController.java
+++ collaborativeorder/frontend/src/main/java/de/skuzzle/cmp/collaborativeorder/orderpage/OrderPageController.java
@@ -2,15 +2,24 @@ package de.skuzzle.cmp.collaborativeorder.orderpage;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import de.skuzzle.cmp.auth.TallyUser;
 
 @Controller
 @RequestMapping("/order")
 public class OrderPageController {
 
-    public OrderPageController() {
-        // TODO Auto-generated constructor stub
-        System.out.println();
+    private final TallyUser currentUser;
+
+    public OrderPageController(TallyUser currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    @ModelAttribute("user")
+    public TallyUser getUser() {
+        return currentUser;
     }
 
     @GetMapping
