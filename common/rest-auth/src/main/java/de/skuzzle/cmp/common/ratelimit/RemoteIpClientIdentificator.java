c0ec0a381e43c2bfaf44069c69bc0bb015c4cdda
Simon Taddiken <simon.taddiken@gmail.com>
2020-03-19 13:23:07 +0100

    fix: more sophisticated approach to finding remote ip

--- common/rest-auth/src/main/java/de/skuzzle/cmp/common/ratelimit/RemoteIpClientIdentificator.java
+++ common/rest-auth/src/main/java/de/skuzzle/cmp/common/ratelimit/RemoteIpClientIdentificator.java
@@ -1,10 +1,19 @@
 package de.skuzzle.cmp.common.ratelimit;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
+
 public class RemoteIpClientIdentificator implements ClientIdentificator<HttpServletRequest> {
+
+    private static final Logger log = LoggerFactory.getLogger(RemoteIpClientIdentificator.class);
 
     @Override
     public Optional<ApiClient> tryIdentifyClientFrom(HttpServletRequest request) {
@@ -13,11 +22,14 @@ public class RemoteIpClientIdentificator implements ClientIdentificator<HttpServ
     }
 
     private String getClientIP(HttpServletRequest request) {
-        final String xrealIp = request.getHeader("X-Real-Ip");
-        if (xrealIp == null) {
-            return request.getRemoteAddr();
-        }
-        return xrealIp;
+        final String xForwardedFor = request.getHeader("X-Forwarded-For");
+        final String xRealIp = request.getHeader("X-Real-Ip");
+        final String remoteAddr = request.getRemoteAddr();
+
+        log.debug("Trying to determine remote ip. X-Real-Ip: {}, X-Forwarded-For: {}, RemoteAddr: {}", xRealIp,
+                xForwardedFor, remoteAddr);
+
+        return Iterables.find(Arrays.asList(xRealIp, xForwardedFor, remoteAddr), Predicates.notNull());
     }
 
 }
