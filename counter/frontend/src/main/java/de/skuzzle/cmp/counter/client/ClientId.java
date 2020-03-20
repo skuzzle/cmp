3660b7fe6527c64414352a80e6c53b903dad83f4
Simon Taddiken <simon.taddiken@gmail.com>
2020-03-20 16:10:14 +0100

    refactor: constants

--- counter/frontend/src/main/java/de/skuzzle/cmp/counter/client/ClientId.java
+++ counter/frontend/src/main/java/de/skuzzle/cmp/counter/client/ClientId.java
@@ -5,16 +5,21 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import com.google.common.net.HttpHeaders;
 
 import de.skuzzle.cmp.auth.TallyUser;
 
 @Component
 class ClientId {
 
+    private static final Logger log = LoggerFactory.getLogger(ClientId.class);
+
     static final String REQUEST_ID = "X-Request-ID";
-    static final String REAL_IP = "X-Real-IP";
-    static final String FORWARTED_FOR = "X-Forwarded-For";
+    static final String REAL_IP = "X-Real-Ip";
 
     private final HttpServletRequest request;
     private final TallyUser tallyUser;
@@ -29,19 +34,20 @@ class ClientId {
     }
 
     public String getForwardedFor() {
-        return Optional.ofNullable(request.getHeader(FORWARTED_FOR))
-                .map(header -> header.split(",")[0])
+        return Optional.ofNullable(request.getHeader(HttpHeaders.X_FORWARDED_FOR))
                 .orElseGet(request::getRemoteAddr);
     }
 
     public String getRequestId() {
         final String requestIdHeaderValue = request.getHeader(REQUEST_ID);
         if (requestIdHeaderValue != null) {
+            log.trace("Found existing request id in HTTP header: {}", requestIdHeaderValue);
             return requestIdHeaderValue;
         }
         String requestId = (String) request.getAttribute(REQUEST_ID);
         if (requestId == null) {
             requestId = UUID.randomUUID().toString();
+            log.debug("Assigned new request id: {}", requestId);
             request.setAttribute(REQUEST_ID, requestId);
         }
         return requestId;
--- counter/frontend/src/main/java/de/skuzzle/cmp/counter/client/ClientIdInterceptor.java
+++ counter/frontend/src/main/java/de/skuzzle/cmp/counter/client/ClientIdInterceptor.java
@@ -2,12 +2,18 @@ package de.skuzzle.cmp.counter.client;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 
+import com.google.common.net.HttpHeaders;
+
 class ClientIdInterceptor implements ClientHttpRequestInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(ClientIdInterceptor.class);
 
     private final ClientId clientId;
 
@@ -19,12 +25,15 @@ class ClientIdInterceptor implements ClientHttpRequestInterceptor {
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
             throws IOException {
 
-        request.getHeaders().add("X-Request-ID", clientId.getRequestId());
-        request.getHeaders().add("X-Real-IP", clientId.getRealIp());
-        request.getHeaders().add("X-Forwarded-For", clientId.getForwardedFor());
+        request.getHeaders().add(ClientId.REQUEST_ID, clientId.getRequestId());
+        request.getHeaders().add(ClientId.REAL_IP, clientId.getRealIp());
+        request.getHeaders().add(HttpHeaders.X_FORWARDED_FOR, clientId.getForwardedFor());
+
+        log.trace("Adding tracing information to backend request. X-Request-ID: {}, X-Real-Ip: {}, X-Forwarded-For: {}",
+                clientId.getRequestId(), clientId.getRealIp(), clientId.getForwardedFor());
 
         clientId.getOidToken().ifPresent(token -> {
-            request.getHeaders().add("Authorization", "Bearer " + token);
+            request.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
         });
 
         return execution.execute(request, body);
