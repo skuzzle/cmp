5edf98590b6676865ee39f6756428dca58be94e6
Simon <Simon@192.168.0.10>
2020-04-06 16:49:25 +0200

    fix: rename class

--- common/spring-boot/src/main/java/de/skuzzle/cmp/spring/boot/properties/FileContentsEnvironmentPostprocessor.java
+++ common/spring-boot/src/main/java/de/skuzzle/cmp/spring/boot/properties/FileContentsEnvironmentPostprocessor.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 
-public class TestEnvironmentPostprocessor implements EnvironmentPostProcessor {
+public class FileContentsEnvironmentPostprocessor implements EnvironmentPostProcessor {
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
