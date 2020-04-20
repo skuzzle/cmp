ac50db0d952e66264c579cd6a2b251bf1d5674ba
Simon Taddiken <simon.taddiken@gmail.com>
2020-04-20 07:30:15 +0200

    auth server stuff

--- users/security/src/main/java/de/skuzzle/cmp/users/security/JwkSetEndpoint.java
+++ users/security/src/main/java/de/skuzzle/cmp/users/security/JwkSetEndpoint.java
@@ -5,13 +5,14 @@ import java.security.Principal;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Map;
 
+import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 
-@RestController
+@FrameworkEndpoint
 public class JwkSetEndpoint {
 
     private final KeyPair keyPair;
@@ -21,6 +22,7 @@ public class JwkSetEndpoint {
     }
 
     @GetMapping("/.well-known/jwks.json")
+    @ResponseBody
     public Map<String, Object> getKey(Principal principal) {
         final RSAPublicKey publicKey = (RSAPublicKey) this.keyPair.getPublic();
         final RSAKey key = new RSAKey.Builder(publicKey).build();
