b633cd695ada3bd017f413eabc1340f1e8c48eb6
Simon Taddiken <simon.taddiken@gmail.com>
2020-05-23 11:05:41 +0200

    Revert the reactive stuff for now

--- counter/frontend/src/main/java/de/skuzzle/cmp/counter/client/BackendClientConfiguration.java
+++ counter/frontend/src/main/java/de/skuzzle/cmp/counter/client/BackendClientConfiguration.java
@@ -1,50 +1,49 @@
 package de.skuzzle.cmp.counter.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Arrays;
+
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClient.Builder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableConfigurationProperties(CmpBackendProperties.class)
 public class BackendClientConfiguration {
 
-    // Nullable to simplify unit test and migration from RestTemplate
-    private final OAuth2AuthorizedClientManager authorizedClientManager;
-
     private final CmpBackendProperties tallyProperties;
+    private final ObjectMapper objectMapper;
     private final ClientId clientId;
 
-    public BackendClientConfiguration(
-            @Autowired(required = false) OAuth2AuthorizedClientManager authorizedClientManager,
-            CmpBackendProperties tallyProperties,
+    public BackendClientConfiguration(CmpBackendProperties tallyProperties, ObjectMapper objectMapper,
             ClientId clientId) {
-        this.authorizedClientManager = authorizedClientManager;
         this.tallyProperties = tallyProperties;
+        this.objectMapper = objectMapper;
         this.clientId = clientId;
     }
 
-    @Bean
-    public WebClient webClient() {
-        final Builder builder = WebClient.builder()
-                .baseUrl(tallyProperties.getUrl());
-        if (authorizedClientManager == null) {
-            return builder.build();
-        }
-        final ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client = new ServletOAuth2AuthorizedClientExchangeFilterFunction(
-                authorizedClientManager);
-        return builder
-                .apply(oauth2Client.oauth2Configuration())
-                .build();
+    private ClientIdInterceptor idInterceptor() {
+        return new ClientIdInterceptor(clientId);
+    }
+
+    private RestTemplate restTemplate(String baseUrl) {
+        final var jacksonMessageConverter = new MappingJackson2HttpMessageConverter(objectMapper);
+        final var restTemplate = new RestTemplate(Arrays.asList(jacksonMessageConverter));
+        final var uriBuilderFactory = new DefaultUriBuilderFactory(baseUrl);
+        restTemplate.setUriTemplateHandler(uriBuilderFactory);
+        restTemplate.getInterceptors().add(idInterceptor());
+        return restTemplate;
     }
 
     @Bean
-    public BackendClient tallyClient(WebClient webClient) {
-        final BackendClient client = new ReactiveBackendClient(webClient);
+    public BackendClient tallyClient() {
+        final BackendClient client = new DefaultBackendClient(
+                restTemplate(tallyProperties.getUrl()),
+                restTemplate(tallyProperties.getHealthUrl()));
         return tallyProperties.useResilienceFeatures()
                 ? new ResilientBackendClient(client)
                 : client;
