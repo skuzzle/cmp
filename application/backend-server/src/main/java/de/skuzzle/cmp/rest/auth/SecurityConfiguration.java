4e7033b91e6c00514a8b8a5e0cddc26225ca3a8a
Simon Taddiken <simon.taddiken@gmail.com>
2020-05-23 12:58:24 +0200

    configure jwks in properties instead of hard coding it

--- application/backend-server/src/main/java/de/skuzzle/cmp/rest/auth/SecurityConfiguration.java
+++ application/backend-server/src/main/java/de/skuzzle/cmp/rest/auth/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,8 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().permitAll().and()
                 .csrf().disable()
-                .oauth2ResourceServer().jwt().jwkSetUri("http://localhost:8082/.well-known/jwks.json").and()
-                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .anonymous();
     }
 
