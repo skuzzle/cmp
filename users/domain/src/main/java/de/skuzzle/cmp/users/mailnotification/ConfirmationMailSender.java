ca82c0023a8b36e356212c81abee12ad638a2fc9
Simon Taddiken <simon.taddiken@gmail.com>
2020-06-18 15:54:35 +0200

    feat: use text blocks

--- users/domain/src/main/java/de/skuzzle/cmp/users/mailnotification/ConfirmationMailSender.java
+++ users/domain/src/main/java/de/skuzzle/cmp/users/mailnotification/ConfirmationMailSender.java
@@ -23,17 +23,20 @@ public class ConfirmationMailSender {
         mailMessage.setFrom("CountMyPizza");
         mailMessage.setTo(e.email());
         mailMessage.setSubject("Your countmy.pizza registration");
-        mailMessage.setText(String.format("Hello %s,\r\n" +
-                "\r\n" +
-                "we have received your registration linked to this E-Mail address. Please visit the link below to\r\n" +
-                "confirm the registration.\r\n" +
-                "\r\n" +
-                "https://auth.countmy.pizza/registration/confirm/%s\r\n" +
-                "\r\n" +
-                "If you haven't registered yourself we kindly ask you to ignore this mail.\r\n" +
-                "\r\n" +
-                "Sincerely\r\n" +
-                "The CMP team",
+        mailMessage.setText(String.format(
+                """
+                        Hello %s,
+
+                        we have received your registration linked to this E-Mail address. Please visit the link below to
+                        confirm the registration.
+
+                        https://auth.countmy.pizza/registration/confirm/%s
+
+                        If you haven't registered yourself we kindly ask you to ignore this mail.
+
+                        Sincerely
+                        The CMP team
+                        """,
                 e.name(), e.confirmationToken()));
         mailSender.send(mailMessage);
     }
@@ -44,17 +47,20 @@ public class ConfirmationMailSender {
         mailMessage.setFrom("CountMyPizza");
         mailMessage.setTo(e.email());
         mailMessage.setSubject("countmy.pizza password reset request");
-        mailMessage.setText(String.format("Hello %s,\r\n" +
-                "\r\n" +
-                "we have received the request to reset your password. Please visit the link below to\r\n" +
-                "reset the password and chose a new onw.\r\n" +
-                "\r\n" +
-                "https://auth.countmy.pizza/resetpw/confirm/%s\r\n" +
-                "\r\n" +
-                "If you haven't requested the reset yourself we kindly ask you to ignore this mail.\r\n" +
-                "\r\n" +
-                "Sincerely\r\n" +
-                "The CMP team",
+        mailMessage.setText(String.format(
+                """
+                        Hello %s,
+
+                        we have received the request to reset your password. Please visit the link below to
+                        reset the password and chose a new one.
+
+                        https://auth.countmy.pizza/resetpw/confirm/%s
+
+                        If you haven't requested the reset yourself we kindly ask you to ignore this mail.
+
+                        Sincerely
+                        The CMP team
+                        """,
                 e.name(), e.confirmationToken()));
     }
 }
