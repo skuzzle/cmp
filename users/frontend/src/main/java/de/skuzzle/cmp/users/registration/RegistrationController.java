b28f2902ab85b1ce55dee9f26c6e8b7badd6f394
Simon Taddiken <simon.taddiken@gmail.com>
2020-06-20 11:46:49 +0200

    feat: prepare some more registration related stuff

--- users/frontend/src/main/java/de/skuzzle/cmp/users/registration/RegistrationController.java
+++ users/frontend/src/main/java/de/skuzzle/cmp/users/registration/RegistrationController.java
@@ -1,9 +1,10 @@
 package de.skuzzle.cmp.users.registration;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RegistrationController {
@@ -16,20 +17,42 @@ public class RegistrationController {
 
     @GetMapping("/registration")
     public String registrationPage() {
-        return "registration/registrationPage";
+        return "registration/registration";
+    }
+
+    @GetMapping("/registration/success")
+    public String registrationSuccessPage() {
+        return "registration/registrationSuccess";
+    }
+
+    @GetMapping("/registration/confirm/{confirmationToken}")
+    public String confirmRegistration(@PathVariable String confirmationToken) {
+        registerUserService.confirmRegistration(confirmationToken);
+
     }
 
     @PostMapping("/registration")
-    public String performRegistration(String name, String email, String password, String passwordRepeat) {
-        if (!passwordRepeat.equals(password)) {
-            throw new RegisterFailedException("Password mismatch");
-        }
-        registerUserService.registerUser(name, email, password);
-        return "redirect:/registration?success";
-    }
+    public ModelAndView performRegistration(String name, String email, String password, String passwordRepeat) {
 
-    @ExceptionHandler(RegisterFailedException.class)
-    public String handleRegisterFailedException(RegisterFailedException e) {
-        return "redirect:/registration?failure";
+        final ModelAndView result = new ModelAndView("registration/registration");
+        result.addObject("name", name);
+        result.addObject("email", email);
+        result.addObject("password", password);
+
+        if (!passwordRepeat.equals(password)) {
+            result.addObject("failure", true);
+            result.addObject("failureMessage", "Registration failed: Passwords do not match");
+            return result;
+        }
+
+        try {
+            registerUserService.registerUser(name, email, password);
+            return new ModelAndView("redirect:/registration/success");
+        } catch (final RegisterFailedException e) {
+            result.addObject("failure", true);
+            result.addObject("failureMessage", "Registration failed: E-Mail address already taken");
+
+            return result;
+        }
     }
 }
