5c5212bf1d2d9ac976b5eca997c3a156c4230572
Simon Taddiken <simon.taddiken@gmail.com>
2021-06-25 12:55:25 +0200

    Fix test

--- counter/rest/src/test/resources/contracts/tally/testGetExistingTallySheetsForUser.groovy
+++ counter/rest/src/test/resources/contracts/tally/testGetExistingTallySheetsForUser.groovy
@@ -5,7 +5,7 @@ import org.springframework.cloud.contract.spec.internal.RegexProperty
 import java.util.regex.Pattern
 
 class Helpers {
-    protected static final String ISO_DATE_TIME_WITH_NANOS = '([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])\\.\\d+'
+    protected static final String ISO_DATE_TIME_WITH_NANOS = Pattern.compile('([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])\\.\\d+');
 
 }
 
@@ -35,11 +35,11 @@ org.springframework.cloud.contract.spec.Contract.make {
                     adminKey: 'adminKey1',
                     createDateUTC: $(
                         consumer('1987-09-12T11:11:00.123'),
-                        producer(regex(Helpers.ISO_DATE_TIME_WITH_NANOS))
+                        producer(regex('([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])\\.\\d+'))
                     ),
                     lastModifiedDateUTC: $(
                         consumer('1987-09-12T11:11:00.123'),
-                        producer(regex(Helpers.ISO_DATE_TIME_WITH_NANOS))
+                        producer(regex('([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])\\.\\d+'))
                     )
                 ]
             ]
